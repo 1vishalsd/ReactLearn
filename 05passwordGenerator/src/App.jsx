be1b54c6c18f00047162d6e6070abbaef4abcb14
1VishalSD <ilearnvk@gmail.com>
2024-06-12 13:57:41 +0530

    start some work on that repo but not a complete

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setLength] = useState(8);
 
+  const [numberAllowed, setNumberAllowed] = useState(false);
+
+  const [charAllowed, setCharAllowed] = useState(false);
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-4xl text-white">Password Generator</h1>
     </>
   );
 }
