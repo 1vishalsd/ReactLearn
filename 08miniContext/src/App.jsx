cf3b328439a7c70c1f03983b303f2b3f657e41cb
1VishalSD <ilearnvk@gmail.com>
2024-06-18 12:36:24 +0530

    add some function

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import UserContextProvider from "./context/userContext";
 
 import "./App.css";
+import Login from "./component/Login";
+import Profile from "./component/Profile";
 
 function App() {
   return (
     <>
       <UserContextProvider>
         <h1>REACT CONtEXT API</h1>
+        <Login />
+        <Profile />
       </UserContextProvider>
     </>
   );
